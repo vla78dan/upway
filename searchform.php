27dfa74ef8715645f3b0b123969a9701efcf45e3
@phpsturm <vla78dan@gmail.com>
2025-01-05 22:27:23 +0300

    Course Website development on WordPress 10_06_01-52-15

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php the_permalink(); ?>">
 
-    <input class="text-search" type="search" name="s" value="" placeholder="Поиск">
+    <input class="text-search" type="search" name="s" value="" name="s" placeholder="Поиск">
     <input type="submit" class="submit-search" value="" />
 
 </form>
